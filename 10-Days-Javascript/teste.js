fc238285e593d06202bcdc3d3216a9b7e6a79439
Adilson <adilsonpjunior@gmail.com>
2021-09-21 17:55:30 -0300

    Challenge day 2

---
+++ 10-Days-Javascript/teste.js
@@ -0,0 +1,11 @@
+const score = 11;
+let grade;
+
+    if (score <= 5) { grade = 'F' }
+    else if (score <= 10) { grade = 'E' }
+    else if (score <= 15) { grade = 'D' }
+    else if (score <= 20) { grade = 'C' }
+    else if (score <= 25) { grade = 'B' }
+    else if (score <= 30) { grade = 'A' }
+
+    console.log(grade);
